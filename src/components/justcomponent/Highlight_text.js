72c7a6178e1fc9e980add785b9dac498a06cd009
Bünyamin Mete <108274379+BunyaminMete@users.noreply.github.com>
2022-08-24 14:10:05 +0300

    Update Highlight_text.js

--- src/components/justcomponent/Highlight_text.js
+++ src/components/justcomponent/Highlight_text.js
@@ -7,20 +7,21 @@ const HighLightText = ({ text, search }) => {
   const patternFinder = (text, search) => {
     const splittedByLetters = text.split('');
     const splittedLetterToLowerCase = text.toLowerCase().split('');
+    const caseSensitiveOffSearch = search.toLowerCase();
 
     let indexPositions = [];
     let found = true;
     //pattern control starts.
     for (let i = 0; i < splittedLetterToLowerCase.length; i++) {
       found = true;
-      for (let j = 0; j < search.length; j++) {
-        if (splittedLetterToLowerCase[i + j] !== search[j]) {
+      for (let j = 0; j < caseSensitiveOffSearch.length; j++) {
+        if (splittedLetterToLowerCase[i + j] !== caseSensitiveOffSearch[j]) {
           found = false;
           break;
         }
       }
       if (found) {
-        for (let x = 0; x < search.length; x++) {
+        for (let x = 0; x < caseSensitiveOffSearch.length; x++) {
           indexPositions.push(i + x);
         }
       }
@@ -30,7 +31,7 @@ const HighLightText = ({ text, search }) => {
     let createNewText = '';
     for (let i = 0; i < splittedByLetters.length; i++) {
       indexPositions.includes(i)
-        ? (createNewText += `<span style='background-color:yellow;'>${splittedByLetters[i]}</span>`)
+        ? (createNewText += `<span style='background-color:orange;'>${splittedByLetters[i]}</span>`)
         : (createNewText += `${splittedByLetters[i]}`);
     }
     console.log(createNewText);
